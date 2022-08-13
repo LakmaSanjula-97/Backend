d43a53fc786a0488a4140686f4e26015a97b3364
LakmaSanjula-97 <lakmagamage97@gmail.com>
2022-08-13 16:45:21 +0530

    update server.js file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8070;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(
