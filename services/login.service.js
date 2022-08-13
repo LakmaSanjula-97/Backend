3e47a90086461ce853e548af36d53d30863278d2
LakmaSanjula-97 <lakmagamage97@gmail.com>
2022-08-13 16:45:37 +0530

    update login service

--- services/login.service.js
+++ services/login.service.js
@@ -7,28 +7,24 @@ const User = require("../models/user.model");
 const login = async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
-  //   const { email, password } = req.body;
 
   try {
-    //checking if the user exists as a registered user
+    //checking if the user exists
     const existingUser = await User.findOne({ email });
 
     if (!existingUser) {
       return res.status(404).json({ message: "User is not Registered." });
     } else {
-      //if user (email) exists in db, the password corresponding to the validated email will be checked with the provided password
-      //comparison of password the user entered and the saved password to verify the login
+      //validate password 
       if (password != existingUser.password) {
         return res.status(400).json({ message: "Incorrect Password." });
       } else {
-        //when the user is validated for the login, a jwt token is created for the login
+        //create jwt token
         const token = jwt.sign(
           { email: existingUser.email, id: existingUser._id },
           "test",
           { expiresIn: "1h" }
         );
-
-        //sending response with status, user details and token
         res
           .status(200)
           .json({ message: "Login Success", result: existingUser, token });
